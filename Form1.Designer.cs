a92bc0a6ec9972f51df209d72b3578caa9cd1c0a
ollyjarvis <olly.m.jarvis@gmail.com>
2022-04-06 22:39:11 +0100

    done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,43 +36,44 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(142, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(524, 277);
+            this.pictureBox1.Size = new System.Drawing.Size(920, 491);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(12, 509);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(390, 143);
+            this.button1.Size = new System.Drawing.Size(417, 160);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(408, 295);
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(516, 509);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(380, 143);
+            this.button2.Size = new System.Drawing.Size(416, 160);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(944, 681);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Revision";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,9 @@ namespace revision
     {
         string rootPath = "C:/Users/olly/Desktop/revision/";
         string rootPathBS = @"C:\Users\olly\Desktop\revision\";
+        int cardNum = 0;
+        string sessionType;
+        string qOrA = "q";
 
         public Form1()
         {
@@ -23,7 +26,6 @@ namespace revision
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
             string session = "0";
 
             if (!Directory.Exists(rootPath +"log/"))
@@ -31,16 +33,25 @@ namespace revision
                 Directory.CreateDirectory(rootPath + "log /");
             }
 
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < Directory.GetDirectories(rootPathBS + @"q").Length + 1; i++)
             {
-                if (!Directory.Exists(rootPath + i + "/q/"))
+                if (!File.Exists(rootPath + "/q/" + i + "/session.txt"))
                 {
-                    Directory.CreateDirectory(rootPath + i + "/q/");
+                    using (FileStream fs = File.Create(rootPath + "/q/" + i + "/session.txt"))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("1");
+                        fs.Write(info, 0, info.Length);
+                        fs.Close();
+                    }
                 }
-
-                if (!Directory.Exists(rootPath + i + "/a/"))
+                if (!File.Exists(rootPath + "/q/" + i + "/done.txt"))
                 {
-                    Directory.CreateDirectory(rootPath + i + "/a/");
+                    using (FileStream fs = File.Create(rootPath + "/q/" + i + "/done.txt"))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("0");
+                        fs.Write(info, 0, info.Length);
+                        fs.Close();
+                    }
                 }
             }
 
@@ -53,6 +64,7 @@ namespace revision
 
                     // Add some information to the file.
                     fs.Write(info, 0, info.Length);
+                    fs.Close();
                 }
             }
 
@@ -65,17 +77,18 @@ namespace revision
                 {
                     session = temp.GetString(b);
                 }
+                fs.Close();
             }
 
             double sessionNum = Int32.Parse(session) + 1;
             string sessionNumToStr = sessionNum.ToString();
-            string sessionType;
 
             using (FileStream fs = File.OpenWrite(rootPath + "/log/log.txt"))
             {
                 Byte[] info = new UTF8Encoding(true).GetBytes(sessionNumToStr);
 
                 fs.Write(info, 0, info.Length);
+                fs.Close();
             }
             
             if (sessionNum / 10 == Math.Floor(sessionNum / 10))
@@ -91,38 +104,180 @@ namespace revision
                 sessionType = "1";
             }
 
-            int directoryCount = Directory.GetDirectories(rootPathBS + sessionType + @"\q").Length;
-            int cardNum = newNum(directoryCount);
-            DisplayImage(cardNum, sessionType);
+            int directoryCount = Directory.GetDirectories(rootPathBS + @"\q").Length;
+            cardNum = newNum(directoryCount);
+            DisplayImage(cardNum, "q");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            int newSession = Int32.Parse(getBox(cardNum)) + 1;
+            if (newSession > 3)
+            {
+                newSession = 3;
+            }
+
+            cardDone(newSession);
+
+            int directoryCount = Directory.GetDirectories(rootPathBS + @"\q").Length;
+            cardNum = newNum(directoryCount);
+            DisplayImage(cardNum, "q");
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
+            int newSession = Int32.Parse(getBox(cardNum)) - 1;
+            if (newSession < 1)
+            {
+                newSession = 1;
+            }
+
+            cardDone(newSession);
+
+            int directoryCount = Directory.GetDirectories(rootPathBS + @"\q").Length;
+            cardNum = newNum(directoryCount);
+            DisplayImage(cardNum, "q");
 
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-
+            if (qOrA == "q")
+            {
+                qOrA = "a";
+            }
+            else if (qOrA == "a")
+            {
+                qOrA = "q";
+            }
+            else
+            {
+                qOrA = "q";
+            }
+            DisplayImage(cardNum, qOrA);
         }
 
-        private void DisplayImage(int number, string session)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int i = 1; i < Directory.GetDirectories(rootPathBS + @"q").Length + 1; i++)
+            {
+                using (FileStream fs = File.OpenWrite(rootPath + "q/" + i + "/done.txt"))
+                {
+                    Byte[] info = new UTF8Encoding(true).GetBytes("0");
+                    fs.Write(info, 0, info.Length);
+                    fs.Close();
+                }
+            }
+        }
+
+        private void DisplayImage(int number, string qOrA)
         {
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            string imageUrl = rootPath + session + "/q/" + number.ToString() + "/page-1.jpg";
+            string imageUrl = rootPath + qOrA + "/" + number.ToString() + "/page-1.jpg";
             pictureBox1.Image = new Bitmap(imageUrl);
         }
 
         private int newNum(int dirNum)
         {
             Random random = new Random();
-            int num = random.Next(0, dirNum);
+            int num = random.Next(1, dirNum);
+            if (getBox(num) == sessionType)
+            {
+                string fileContent = "";
+                using (FileStream fs = File.OpenRead(rootPath + "q/" + num + "/done.txt"))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        fileContent = temp.GetString(b);
+                    }
+                    fs.Close();
+                }
+                string fileContStr = fileContent.Replace("\0", string.Empty);
+                if (fileContStr == "0")
+                {
+                    return num;
+                }
+            }
+
+            for (int i = 1; i < dirNum; i++)
+            {
+                string fileContent = "";
+                using (FileStream fs = File.OpenRead(rootPath + "q/" + i + "/session.txt"))
+                {
+                    byte[] b = new byte[1024];
+                    UTF8Encoding temp = new UTF8Encoding(true);
+
+                    while (fs.Read(b, 0, b.Length) > 0)
+                    {
+                        fileContent = temp.GetString(b);
+                    }
+                    fs.Close();
+                }
+                string fileContStr = fileContent.Replace("\0", string.Empty);
+                if (fileContStr == sessionType)
+                {
+                    string fileContent2 = "";
+                    using (FileStream fs = File.OpenRead(rootPath + "q/" + i + "/done.txt"))
+                    {
+                        byte[] b = new byte[1024];
+                        UTF8Encoding temp = new UTF8Encoding(true);
+
+                        while (fs.Read(b, 0, b.Length) > 0)
+                        {
+                            fileContent2 = temp.GetString(b);
+                        }
+                        fs.Close();
+                    }
+                    string fileContStr2 = fileContent2.Replace("\0", string.Empty);
+                    if (fileContStr2 == "0")
+                    {
+                        return i;
+                    }
+                }
+            }
             return num;
         }
+
+        private void cardDone(int newSession)
+        {
+            using (FileStream fs = File.OpenWrite(rootPath + "q/" + cardNum + "/done.txt"))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("1");
+                fs.Write(info, 0, info.Length);
+                fs.Close();
+            }
+
+            using (FileStream fs = File.OpenWrite(rootPath + "q/" + cardNum + "/session.txt"))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes(newSession.ToString());
+                fs.Write(info, 0, info.Length);
+                fs.Close();
+            }
+
+        }
+
+        private string getBox(int num)
+        {
+            string box = "";
+            using (FileStream fs = File.OpenRead(rootPath + "q/" + num.ToString() + "/session.txt"))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    box = temp.GetString(b);
+                }
+                fs.Close();
+            }
+            string output = box.Replace("\0", string.Empty);
+            return output;
+        }
+
+
     }
 }
